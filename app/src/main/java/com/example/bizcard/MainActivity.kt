6659a2e6c4d9fa94239d0bca6a9d27deb6e4516a
Aaron <toscleveland@gmail.com>
2023-03-11 19:13:40 -0600

    Init Project

---
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.bizcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bizcard.ui.theme.BizCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BizCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    CreateBizCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateBizCard() {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(390.dp)
+            .padding(15.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 6.dp) {
+
+            Surface(modifier = Modifier
+                .size(150.dp)
+                .padding(5.dp), shape = CircleShape, border = BorderStroke(width = 0.5.dp, color = Color.LightGray),
+                elevation = 4.dp, color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)) {
+
+                Image(painter = painterResource(id = R.drawable.profile_image),
+                    contentDescription = "Profile Image", modifier = Modifier.size(135.dp),
+                    contentScale = ContentScale.Crop)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BizCardTheme {
+        CreateBizCard()
+    }
+}
